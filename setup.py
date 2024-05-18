c8c36273deb529c8866c2f3a238aceede82d8eca
HukovychB <72617770@fsv.cuni.cz>
2024-05-18 21:49:00 +0200

    Added a .py file

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     version="0.1",
     description="Web application for applying technical analysis on financial instruments",
     author="Bohdan Hukovych & Jaroslav Louda",
-    author_email="72617770@fsv.cuni.cz",
+    author_email="72617770@fsv.cuni.cz & 45673694@fsv.cuni.cz",
     url="https://github.com/HukovychB/TA-Project-2024",
 )
