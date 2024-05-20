38bd44f8e996e0821eef633232b757373afbc8b0
HukovychB <72617770@fsv.cuni.cz>
2024-05-21 00:02:27 +0200

    some small changes

--- app/frontend/web.py
+++ app/frontend/web.py
@@ -1,12 +1,18 @@
 import streamlit as st
 
+st.set_page_config(page_title="TA App",
+                page_icon=":chart_with_upwards_trend:",
+                layout="wide"
+)
+import sys
+sys.path.append("C:/Files/study/semestr6/TA project")
+from app.backend import main as m
+
 #Colors
 #Background: #3f3b3b
 #Text: #f2e1e1
 #Other elements: #FF5500
 #Font: Serif
-
-st.set_page_config(layout="wide")
 
 st.image("app/frontend/ies.png", width=100)
 
@@ -16,6 +22,7 @@ st.write("")
 st.write("")
 
 ticker_input = st.text_input("Enter the ticker (e.g., AAPL, MSFT):")
+m.ticker_input = ticker_input
 
 
 
