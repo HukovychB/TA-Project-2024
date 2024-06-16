e8386b5bd5c722006c3adc10a0a494d24eb9b7d5
HukovychB <72617770@fsv.cuni.cz>
2024-06-16 15:32:26 +0200

    Bugs

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,6 +1,7 @@
 import yfinance as yf
-import sys
-sys.path.append("C:/Files/study/semestr6/TA project")
-from app.frontend import web as w
+
+#import sys
+#sys.path.append("C:/Files/study/semestr6/TA project")
+#from app.frontend import web as w
 
 price = yf.Ticker(ticker_input)
