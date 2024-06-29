dd08bec5363ae3e0352f9704b2805c0eb51cc313
HukovychB <72617770@fsv.cuni.cz>
2024-06-29 10:06:21 +0200

    Changed the folder structure, temporarily commented some parts of the code, added interactive graph

--- app/main.py
+++ app/main.py
@@ -1,10 +1,59 @@
 import yfinance as yf
-from frontend import web
+import streamlit as st
+import plotly.graph_objs as go
 
-print(f"This is the ticker {web.ticker_input}")
+def fetch_data(ticker_input):
+    ticker_data = yf.Ticker(ticker_input)
+    price = ticker_data.history(period = 'max')
+    if not price.empty:
+        return ticker_data
+    else:
+        st.error("Error: The ticker is not recognized. Please provide a valid symbol listed on Yahoo Finance (https://finance.yahoo.com/).", icon="🚨")
 
-#import sys
-#sys.path.append("C:/Files/study/semestr6/TA project")
-#from app.frontend import web as w
+def create_graph(ticker_data, period):
+    price_df = ticker_data.history(period = period)
+    fig = go.Figure()
+    fig.add_trace(go.Candlestick(x=price_df.index,
+                                open=price_df['Open'],
+                                high=price_df['High'],
+                                low=price_df['Low'],
+                                close=price_df['Close'],
+                                name='Candles',
+                                increasing_line_color='lightgreen',
+                                decreasing_line_color='orangered',   
+                                whiskerwidth=0.1))
+    fig.update_layout(
+        title=f"Graph {ticker_data.info.get('longName')}",
+        xaxis_rangeslider_visible=False,
+        title_font=dict(size=32, family='serif', color='linen'),
+        height=600,
+        dragmode='pan',
+        uirevision='constant',
+        xaxis=dict(
+            showline=False,
+            linecolor='dimgrey',            
+            gridcolor='black',
+            rangebreaks=[                 
+            dict(bounds=["sat", "mon"])
+            ],
+            tickfont=dict(family='serif', size=12, color='linen')                
+        ),
+        yaxis=dict(
+        showline=False,
+        linecolor='dimgrey',            
+        gridcolor='dimgrey',
+        tickfont=dict(family='serif', size=12, color='linen')               
+        )
+    )
 
-#price = yf.Ticker(ticker_input)
+    st.plotly_chart(fig, config=dict(scrollZoom=True))
+
+if __name__ == '__main__':
+    stock = yf.Ticker("AAPL")
+    stock.history(period = 'max')
+
+
+
+    
+
+    
